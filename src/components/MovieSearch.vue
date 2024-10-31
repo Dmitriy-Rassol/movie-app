33c8ecd1ceeb4d302d130fcf6ec941ed5ce168cc
Dmitriy Rassolenko <rassoldima@yandex.ru>
2024-11-01 00:25:23 +0300

    Загружаем все фильмы при монтировании компонента

--- src/components/MovieSearch.vue
+++ src/components/MovieSearch.vue
@@ -1,37 +1,54 @@
 <template>
-    <div>
-      <input
-        v-model="searchQuery"
-        placeholder="Поиск фильмов"
-        @keydown.enter="searchMovies" />
-      <button @click="searchMovies">Искать</button>
-      <div v-if="movies.length">
-        <h2>Результаты поиска:</h2>
-        <ul>
-          <li v-for="movie in movies" :key="movie.id">
+  <div>
+    <input
+      v-model="searchQuery"
+      placeholder="Поиск фильмов"
+      @keydown.enter="searchMovies"
+    />
+    <button @click="searchMovies">Искать</button>
+    <div v-if="movies.length">
+      <h2>Результаты поиска:</h2>
+      <ul>
+        <li v-for="movie in movies" :key="movie.id">
+         
             {{ movie.name }} ({{ movie.year }})
-          </li>
-        </ul>
-      </div>
+            <img :src="movie.poster.url" alt="" />
+          
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, computed } from 'vue';
-  import { useMovieStore } from '@/stores/movieStore'; // Импортируем хранилище
-  
-  
-      const movieStore = useMovieStore(); // Инициализируем хранилище
-      const searchQuery = ref('');
-      
-      const searchMovies = async () => {
-        await movieStore.searchMovies(searchQuery.value); // Вызываем действие из хранилища
-      };
+    <div v-else>
+      <ul>
+        <li v-for="movie in allMovies" :key="movie.id">
+          
+            {{ movie.name }} ({{ movie.year }})
+            <img :src="movie.poster.url" alt="" />
+         
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
 
-      const movies = computed(() => movieStore.movies);
-  </script>
-  
-  <style scoped>
-  /* Добавьте стили по вашему усмотрению */
-  </style>
-  
+<script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+import { useMovieStore } from "../stores/movieStore"; // Импортируем хранилище
+
+const movieStore = useMovieStore(); // Инициализируем хранилище
+const searchQuery = ref("");
+
+onMounted(async () => {
+  await movieStore.fetchAllMovies(); // Загружаем все фильмы при монтировании компонента
+});
+
+const searchMovies = async () => {
+  await movieStore.searchMovies(searchQuery.value); // Вызываем действие из хранилища
+};
+
+const movies = computed(() => movieStore.movies);
+const allMovies = computed(() => movieStore.allMovies);
+</script>
+
+<style scoped>
+/* Добавьте стили по вашему усмотрению */
+</style>
--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -1,17 +1,18 @@
 
 // src/stores/movieStore.ts
 import { defineStore } from 'pinia';
-import { fetchMovies } from '../api';
+import { fetchSearchMovies, fetchMovies } from '../api';
 
 export const useMovieStore = defineStore('movie', {
   state: () => ({
-    movies: [] as Array<{ id: number; name: string; year: number }>,
+    movies: [] as Array<{ id: number; name: string; year: number; poster: { url: string} }>,
+    allMovies: [] as Array<{ id: number; name: string; year: number; poster: { url: string }}>,
   }),
   actions: {
     async searchMovies(query: string) {
       if (query.trim()) {
         try {
-          const data = await fetchMovies(query);
+          const data = await fetchSearchMovies(query);
           console.log(data.docs);
           
           this.movies = data.docs; // Предполагаем, что данные находятся в docs
@@ -19,6 +20,16 @@ export const useMovieStore = defineStore('movie', {
           console.error('Ошибка при поиске фильмов:', error);
         }
       }
+    },
+    async fetchAllMovies () {
+        try {
+          const data = await fetchMovies();
+          console.log(data.docs);
+          
+          this.allMovies = data.docs; // Предполагаем, что данные находятся в docs
+        } catch (error) {
+          console.error('Ошибка при поиске фильмов:', error);
+        }
     },
   },
 });
